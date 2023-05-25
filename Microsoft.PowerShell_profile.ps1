36a2d2298b8dc8e683b860f37e27bcaa5a604efd
Trafalgar <1148636359g@gmail.com>
2023-05-25 15:14:53 +0800

    添加了在windows下push android程序并在android shell中执行的函数 改变了默认的encoding, 现在可以正常输出utf8编码字符

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,7 @@
 
+#set encoding
+$OutputEncoding = [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
+
 # Import-Module DirColors
 Import-Module posh-git
 Import-Module PSReadLine
@@ -16,10 +19,11 @@ Set-PSReadlineKeyHandler -Key "Ctrl+z" -Function Undo
 Set-PSReadlineKeyHandler -Key UpArrow  -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
-Set-PSReadlineKeyHandler -Key "Ctrl+a" -Function BeginningOfLine 
-Set-PSReadlineKeyHandler -Key "Ctrl+e" -Function EndOfLine 
+# Set-PSReadlineKeyHandler -Key "Ctrl+a" -Function BeginningOfLine 
+# Set-PSReadlineKeyHandler -Key "Ctrl+e" -Function EndOfLine 
 
-# 不好用
+Set-PSReadLineOption -EditMode Emacs
+# vi模式不好用 没有光标转换
 # Set-PSReadLineOption -EditMode Vi
 # Set-PSReadlineOption -ViModeIndicator Cursor
 
@@ -27,6 +31,11 @@ Set-PSReadlineKeyHandler -Key "Ctrl+e" -Function EndOfLine
 $env:all_proxy="socks5://127.0.0.1:7890"
 $env:NDK = "C:\Users\guoya\AppData\Local\Android\Sdk\ndk\25.1.8937393"
 Set-Alias  vi nvim-qt
+
+Set-Alias android_clang C:/Users/guoya/AppData/Local/Android/Sdk/ndk/25.1.8937393/toolchains/llvm/prebuilt/windows-x86_64/bin/aarch64-linux-android29-clang
+Set-Alias android_clang++ C:/Users/guoya/AppData/Local/Android/Sdk/ndk/25.1.8937393/toolchains/llvm/prebuilt/windows-x86_64/bin/aarch64-linux-android29-clang++
+
+Set-Alias rename Rename-Item
 
 #设置socks5代理
 function set_proxy {
@@ -52,3 +61,24 @@ function unset_proxy{
 function set_pwsh{
  gvim $PROFILE
 }
+
+# push file to android and run
+function android_prun($exec){
+  write-host $exec
+  adb push "$exec" /data/local/tmp/"$exec"
+  adb shell chmod 777 /data/local/tmp/"$exec"
+  adb shell /data/local/tmp/"$exec"
+}
+
+# run file in android
+function android_run($exec){
+  write-host $exec
+  adb shell /data/local/tmp/"$exec"
+}
+
+function msvc_run{
+  $prefix="D:\workspace\dev\vcpkg\installed\x64-windows"
+  $env:PATH+=";${prefix}\bin"
+  $env:PATH+=";${prefix}\debug\bin"
+}
+
